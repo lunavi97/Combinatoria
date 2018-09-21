5655c475485b7e18fa4b6f1a447c87ee9e1ce6a5
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-21 20:30:39 -0300

    Implementado método variación sin repetición

---
+++ src/main/java/com/udemy/Combinatoria/CombinatoriaCalc.java
@@ -0,0 +1,23 @@
+package com.udemy.Combinatoria;
+
+public class CombinatoriaCalc {
+
+  /**
+   * Número de variaciones de un conjunto de m elementos tomados
+   * en tuplas de n elementos, sin elementos repetidos.
+   * @param m - cantidad de elementos del universo
+   * @param n - cantidad de elementos de las tuplas
+   * @return cantidad de tuplas
+   */
+  public int variacionSinRepeticion(int m, int n) {
+    if (n > m) {
+      throw new RuntimeException("Cantidad de elementos de tuplas mayor que las del universo");
+    }
+
+    int res = m;
+    for (int i = 1; i < n; i++) {
+      res *= --m;
+    }
+    return res;
+  }
+}
---
+++ src/test/java/com/udemy/Combinatoria/VariacionTest.java
@@ -0,0 +1,44 @@
+package com.udemy.Combinatoria;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class VariacionTest {
+  
+  private CombinatoriaCalc cCalc;
+
+  @Before
+  public void setUp() throws Exception {
+    cCalc = new CombinatoriaCalc();
+  }
+
+  // Variaciones sin repetición
+  
+  @Test(expected = RuntimeException.class)
+  public void throwsRuntimeException_IfMIs9AndNIs11() {
+    cCalc.variacionSinRepeticion(9, 11);
+  }
+  
+  @Test
+  public void returns336_IfMIs8AndNIs3() {
+    Assert.assertEquals(336, cCalc.variacionSinRepeticion(8, 3));
+  }
+  
+  @Test
+  public void returns93024_IfMIs19AndNIs4() {
+    Assert.assertEquals(93024, cCalc.variacionSinRepeticion(19, 4));
+  }
+  
+  @Test
+  public void returns12_IfMIs4AndNIs2() {
+    Assert.assertEquals(12, cCalc.variacionSinRepeticion(4, 2));
+  }
+  
+  @Test
+  public void returns720_IfMAndNAre6() {
+    Assert.assertEquals(720, cCalc.variacionSinRepeticion(6, 6));
+  }
+
+}
