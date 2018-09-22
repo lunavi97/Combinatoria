a2671fa8dd0a93d46fc4ccd6315886042f00995c
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-21 22:57:46 -0300

    Creado test suite

---
+++ src/test/java/com/udemy/Combinatoria/AllTests.java
@@ -0,0 +1,11 @@
+package com.udemy.Combinatoria;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CombinacionTest.class, PermutacionTest.class, VariacionTest.class })
+public class AllTests {
+
+}
